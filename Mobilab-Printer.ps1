d5fe7b50195ee45195a4746e22b67d9c6faea3b7
Michael <mikev83@gmail.com>
2024-06-06 10:10:12 +0200

    Add files via upload

---
+++ Mobilab-Printer.ps1
@@ -0,0 +1,18 @@
+# Define printer name and printer path
+$printerName = "Mobilab-Printer"
+$printerPath = "ipp://192.168.1.8"
+
+# Check if the printer exists
+$printer = Get-Printer -Name $printerName -ErrorAction SilentlyContinue
+
+# If the printer exists, remove it
+if ($printer) {
+    Remove-Printer -Name $printerName
+}
+
+# Small delay to ensure the removal completes
+Start-Sleep -Seconds 2
+
+# Add the printer
+Add-Printer -Name $printerName -DriverName "Microsoft IPP Class Driver" -PortName $printerPath
+Set-Printer -Name $printerName -Comment "HR_Printer_Privileged" -Shared $true -Published $true
