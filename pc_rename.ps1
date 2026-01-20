f6ac9f1bfb5ebd4981814f0fb4c72e386cdb3a29
Michael <mikev83@gmail.com>
2026-01-20 10:59:52 -0800

    Modify computer rename logic to use short serial
    Updated the computer renaming script to use a shortened serial number for the new name and removed unnecessary spaces.

--- pc_rename.ps1
+++ pc_rename.ps1
@@ -1,17 +1,23 @@
 # Get current computer name
 $currentName = $env:COMPUTERNAME
 
-# Get serial number using CIM (WMI replacement)
+# Get serial number using CIM
 $serial = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
 
-# Build new name
-$newName = "AKO-$serial"
+# Clean serial (remove spaces just in case)
+$serial = $serial -replace '\s',''
 
-# Check if rename is needed
+# Max NetBIOS length = 15
+$maxSerialLength = 11
+$shortSerial = $serial.Substring(0, [Math]::Min($serial.Length, $maxSerialLength))
+
+# Build new name
+$newName = "AKO-$shortSerial"
+
+# Rename only if needed
 if ($currentName -ne $newName) {
     Write-Output "Renaming computer from $currentName to $newName"
-
-    Rename-Computer -NewName $newName -Force -Restart
+    Rename-Computer -NewName $newName -Force
 }
 else {
     Write-Output "Computer name already correct: $currentName"
